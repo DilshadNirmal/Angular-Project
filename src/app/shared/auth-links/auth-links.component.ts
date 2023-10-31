6f16e161d9a25c762d5987b1bbc9cf097ed843f6
DilshadNirmal <dilshadgov2020@gmail.com>
2023-10-31 14:05:07 +0530

    angular-project

--- src/app/shared/auth-links/auth-links.component.ts
+++ src/app/shared/auth-links/auth-links.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-auth-links',
   templateUrl: './auth-links.component.html',
   styleUrls: ['./auth-links.component.css']
 })
-export class AuthLinksComponent {
+export class AuthLinksComponent implements OnInit {
 
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
 }
