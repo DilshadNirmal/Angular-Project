3e3e41a56bb58c3c6d0b34ae127b809990bfc74a
DilshadNirmal <dilshadgov2020@gmail.com>
2023-11-01 14:27:17 +0530

    new components created

---
+++ src/app/shared/card/course-footer/course-footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-footer',
+  templateUrl: './course-footer.component.html',
+  styleUrls: ['./course-footer.component.css']
+})
+export class CourseFooterComponent {
+
+}
