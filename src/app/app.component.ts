e89e478ca24c847bc159b001ef9e01315322b6fd
sabitha <sabithagandhinehru1691@gmail.com>
2023-11-01 15:36:34 +0530

    simiar changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,31 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Project';
 
+  isMenuScrolled = false;
+  isSidebarShowing = false;
+
+  @HostListener('window:scroll', ['$event'])
+  scrollCheck() {
+    if (window.pageYOffset > 100)
+      this.isMenuScrolled = true;
+    else
+      this.isMenuScrolled = false;
+
+
+    // console.log(this.isMenuScrolled)
+  }
+
+  openSideBar() {
+    this.isSidebarShowing = true;
+  }
+
+  closeSideBar() {
+    this.isSidebarShowing = false;
+  }
+
+  scrollToTop() {
+    document.body.scrollIntoView({ behavior: 'smooth' })
+  }
 }
+
