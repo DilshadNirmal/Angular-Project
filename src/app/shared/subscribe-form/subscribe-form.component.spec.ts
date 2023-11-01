e9301535dfbc382add2d2e212e5d32694e6d20f9
DilshadNirmal <dilshadgov2020@gmail.com>
2023-11-01 19:37:58 +0530

    lastCommit by me Ms.Sabitha

---
+++ src/app/shared/subscribe-form/subscribe-form.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SubscribeFormComponent } from './subscribe-form.component';
+
+describe('SubscribeFormComponent', () => {
+  let component: SubscribeFormComponent;
+  let fixture: ComponentFixture<SubscribeFormComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [SubscribeFormComponent]
+    });
+    fixture = TestBed.createComponent(SubscribeFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
