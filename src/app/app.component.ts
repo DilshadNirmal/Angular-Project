1ad18586e8817c523f504272072bc93ee029c808
DilshadNirmal <dilshadgov2020@gmail.com>
2023-11-01 15:33:42 +0530

    nav&Header updated in app.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,32 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Project';
 
+  title = ''
+
+  isMenuScrolled = false;
+  isSidebarShowing = false;
+
+  @HostListener('window:scroll', ['$event'])
+  scrollCheck() {
+    if (window.pageYOffset > 100)
+      this.isMenuScrolled = true;
+    else
+      this.isMenuScrolled = false;
+
+
+    // console.log(this.isMenuScrolled)
+  }
+
+  openSideBar() {
+    this.isSidebarShowing = true;
+  }
+
+  closeSideBar() {
+    this.isSidebarShowing = false;
+  }
+
+  scrollToTop() {
+    document.body.scrollIntoView({ behavior: 'smooth' })
+  }
 }
